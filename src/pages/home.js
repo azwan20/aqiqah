bc41987b7e2326c5c35e91aad8468e7fb633948b
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2024-06-09 18:47:24 +0800

    Add files via upload

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,15 @@
-export default function Homes({handleNavbar}) {
-    return(
+export default function Homes({ handleNavbar }) {
+    return (
         <>
             <div className="homes">
-                <h5 className="text-uppercase grid" data-aos="flip-right">undangan tasyakuran aqiqah</h5>
+                <h5 className="text-uppercase grid" data-aos="flip-right" style={{padding: '0 50px'}}>undangan tasyakuran aqiqah</h5>
                 <div className="imgHome my-3" data-aos="flip-up">
                     <img src="https://www.gap.com/Asset_Archive/GPWeb/content/0030/014/459/assets/mob/SU235172_GapBaby_img_MOB.jpg" width={100} />
                 </div>
-                <img className="forHome" src="/forHome.png" />
-                <h4 className="my-5" data-aos="flip-up">Muhammad Hilmi Nor Fauzy</h4>
-                <span data-aos="flip-right">
+                <div className="forHome">
+                    <h4 className="my-5" data-aos="flip-up">Muhammad Hilmi Nor Fauzy</h4>
+                </div>
+                <span data-aos="flip-right" style={{padding: '0 50px'}}>
                     <p>Kepada Yth.</p>
                     <p>Bapak/Ibu</p>
                     <h5 className="mt-4">Fitrah Asfar S.kom</h5>
